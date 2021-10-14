e00f2ba03acfbb1bad198dd192accae7431ab18d
Victor Bjergby <89514123+Hansivboy@users.noreply.github.com>
2021-10-14 11:36:48 +0200

    Login fixed

--- Bankapp/Bank.py
+++ Bankapp/Bank.py
@@ -15,39 +15,39 @@ collection = db["Kunder"]
 
 #Åbner vores gui - Main Screen, og kalder den Bank App 
 master = Tk()
-master.title('Bank App')
+master.title('Project')
 
 #Funktioner
 #Definere funktion for kunde oprettelse
 def finish_reg():
-    cpr = temp_cpr.get() #Definere variabler, som information som systemet får af kunden ved hjælp af .get() funktion
+    email = temp_email.get() #Definere variabler, som information som systemet får af kunden ved hjælp af .get() funktion
     name = temp_name.get()
     age = temp_age.get()
     gender = temp_gender.get()
     password = temp_password.get()
 
-    if cpr == "" or name == "" or age == "" or gender == "" or password == "": #Sikre kunden udfylder alle felter.
+    if email == "" or name == "" or age == "" or gender == "" or password == "": #Sikre kunden udfylder alle felter.
         notif.config(fg="red",text="Please udfyld alle oplysninger * ") #Hvis et af felterne er tomme, skrives dette til kunden
         return
-    if collection.find_one({'_id': cpr}): #sikre at kunden ikke allerede eksistere i systemet. Dette gøres ved hjælp af find_one, på id's
+    if collection.find_one({'_id': email}): #sikre at kunden ikke allerede eksistere i systemet. Dette gøres ved hjælp af find_one, på id's
         notif.config(fg="red",text="Account eksisterer allerede")
         return
     else:
         #Hvis kunden har udfyldt alle felter, og ikke allerede eksisterer, så oprettes kunden i databasen.
-        mydict = {"_id": cpr, "Navn": name, "Alder": age, "Gender": gender, "Password": password, "Balance": "0"}
+        mydict = {"_id": email, "Navn": name, "Alder": age, "Gender": gender, "Password": password, "Balance": "0"}
         collection.insert_one(mydict)
         notif.config(fg="green", text="Bruger oprettet")
 
 #Ny variable defineres som register
 def register():
     #Definere variabler som skal bruges på login skærmen
-    global temp_cpr
+    global temp_email
     global temp_name
     global temp_age
     global temp_gender
     global temp_password
     global notif
-    temp_cpr = StringVar()
+    temp_email = StringVar()
     temp_name = StringVar()
     temp_age =  StringVar()
     temp_gender = StringVar()
@@ -59,7 +59,7 @@ def register():
 
     #Laver labels på skærmen
     Label(register_screen, text="Udfyldt dine detaljer her, for at opret dig", font=('Calibri',12)).grid(row=0,sticky=N,pady=10)
-    Label(register_screen, text="CPR", font=('Calibri',12)).grid(row=1,sticky=W)
+    Label(register_screen, text="Email", font=('Calibri',12)).grid(row=1,sticky=W)
     Label(register_screen, text="Navn", font=('Calibri',12)).grid(row=2,sticky=W)
     Label(register_screen, text="Alder", font=('Calibri',12)).grid(row=3,sticky=W)
     Label(register_screen, text="Gender", font=('Calibri',12)).grid(row=4,sticky=W)
@@ -68,7 +68,7 @@ def register():
     notif.grid(row=7,sticky=N,pady=10)    
 
     #Entries
-    Entry(register_screen, textvariable=temp_cpr) .grid(row=1, column=1,padx=5)
+    Entry(register_screen, textvariable=temp_email) .grid(row=1, column=1,padx=5)
     Entry(register_screen, textvariable=temp_name) .grid(row=2, column=1,padx=5)
     Entry(register_screen, textvariable=temp_age) .grid(row=3, column=1,padx=5)
     Entry(register_screen, textvariable=temp_gender) .grid(row=4, column=1,padx=5)
@@ -92,13 +92,13 @@ def clock():
 
 
 def login_session():
-    global login_cpr
-    login_cpr = temp_login_cpr.get()
+    global login_email
+    login_email = temp_login_email.get()
     login_password = temp_login_password.get()
     #Cpr nummer skal stemme overens med et id, og med Password
-    if collection.find_one({'_id': login_cpr}) and collection.find_one({'Password': login_password}):
+    if collection.find_one({'_id': login_email}) and collection.find_one({'Password': login_password}):
         #Finder denne kundes navn, ved hjælp af cpr nummer
-        name = collection.find_one({'_id': login_cpr}, {'Navn': 1, '_id': 0})
+        name = collection.find_one({'_id': login_email}, {'Navn': 1, '_id': 0})
         navn = name['Navn']
         #Åbner en ny skærm efter login er successfuldt 
         login_screen.destroy()
@@ -162,16 +162,16 @@ def FacebookStock():
 
 def personal_details():
     #Definere de foskellige columns i databasen som variabler
-    details_name = collection.find({'_id': login_cpr}, {'Navn': 1, '_id': 0})
+    details_name = collection.find({'_id': login_email}, {'Navn': 1, '_id': 0})
     for names in details_name:
        name = names['Navn']
-    details_age = collection.find({'_id': login_cpr}, {'Alder': 1, '_id': 0})
+    details_age = collection.find({'_id': login_email}, {'Alder': 1, '_id': 0})
     for alder in details_age:
         age = alder['Alder']
-    details_gender = collection.find({'_id': login_cpr}, {'Gender': 1, '_id': 0})
+    details_gender = collection.find({'_id': login_email}, {'Gender': 1, '_id': 0})
     for gender in details_gender:
         gen = gender['Gender']
-    details_balance = collection.find({'_id': login_cpr}, {'Balance': 1, '_id': 0})
+    details_balance = collection.find({'_id': login_email}, {'Balance': 1, '_id': 0})
     for balance in details_balance:
         penge = balance['Balance']
     #Personal Details Screen
@@ -185,23 +185,23 @@ def personal_details():
     Label(personal_details_screen, text="Balance : "+str(penge)+"DKK",font=('Calibri',12)).grid(row=4, sticky=W)
 def login():
     #variabler
-    global temp_login_cpr
+    global temp_login_email
     global temp_login_password
     global login_notif
     global login_screen
-    temp_login_cpr = StringVar()
+    temp_login_email = StringVar()
     temp_login_password = StringVar()
     #Login Screen
     login_screen = Toplevel(master)
     login_screen.title('Login')
     #Labels
     Label(login_screen, text="Login til din account", font=('Calibri',12)).grid(row=0,sticky=N,pady=10)
-    Label(login_screen, text="CPR", font=('Calibri',12)).grid(row=1,sticky=W,pady=10)
+    Label(login_screen, text="Email", font=('Calibri',12)).grid(row=1,sticky=W,pady=10)
     Label(login_screen, text="Password", font=('Calibri',12)).grid(row=2,sticky=W,pady=10)
     login_notif = Label(login_screen, font=('Calibri',12))
     login_notif.grid(row=4,sticky=N)
     #Entry
-    Entry(login_screen, textvariable=temp_login_cpr).grid(row=1,column=1,padx=5)
+    Entry(login_screen, textvariable=temp_login_email).grid(row=1,column=1,padx=5)
     Entry(login_screen, textvariable=temp_login_password,show='*').grid(row=2,column=1,padx=5)
     #Button
     Button(login_screen, text="Login", command=login_session, width=15,font=('Calibri',12)).grid(row=3,sticky=W,pady=5,padx=5)
@@ -235,7 +235,7 @@ def AllStocks():
 
 
 #Image import
-img = Image.open('Secure.png')
+img = Image.open('raspberry.png')
 img = img.resize((250,150))
 img = ImageTk.PhotoImage(img)
 
@@ -264,8 +264,8 @@ Stocksimage = Stocksimage.resize((150,150))
 Stocksimage = ImageTk.PhotoImage(Stocksimage)
 
 #Labels
-Label(master, text = "Bank 06", font=('Calibri',14)).grid(row=0,sticky=N,pady=10)
-Label(master, text = "Sikker Bank", font=('Calibri',12)).grid(row=1,sticky=N)
+Label(master, text = "Group x", font=('Calibri',14)).grid(row=0,sticky=N,pady=10)
+Label(master, text = "Raspberry PI", font=('Calibri',12)).grid(row=1,sticky=N)
 Label(master, image=img).grid(row=2,sticky=N,pady=15)
 
 
